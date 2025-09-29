83a20b0d1efcd6c639a8dd376d818a9d882306d4
Martin <martin.uni.2024@gmail.com>
2025-09-28 23:42:16 -0500

    Agrega script de colaboración

---
+++ Actividad6-CC3S2/colaboracion.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Script de colaboración para demostrar trabajo en equipo
+"""
+
+def trabajo_colaborativo():
+    print('¡La colaboración es clave!')
+    print('Trabajando en equipo logramos más')
+
+if __name__ == '__main__':
+    trabajo_colaborativo()
